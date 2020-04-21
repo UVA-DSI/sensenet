b998f8263db2904ed9729a745a1819edb1808144
krad246 <krad246@gmail.com>
2020-04-21 16:59:15 -0400

    added some gps parsing code as outlined in the adafruit example. did not touch the payload data at all but set up some software to acquire a fix at boot, then put the gps into low power mode

---
+++ sensorboxes/sbox-gps-test/ttn-credentials.h
@@ -0,0 +1,14 @@
+/* 
+ *
+ * TTN credentials
+ * 
+ */
+
+// application id 
+static const u1_t PROGMEM APPEUI[8]= { FILL ME };
+
+// dev id 
+static const u1_t PROGMEM DEVEUI[8]= { FILL ME };
+
+// application key
+static const u1_t PROGMEM APPKEY[16] = { FILL ME };
