78bba65bec3255c65bcd3afcec1c66e0e6d6abf6
krad246 <krad246@gmail.com>
2020-04-24 00:05:25 -0400

    updated and verified GPS code. can't get a fix from inside my room, but it does correctly make attempts to get a fix. added a limit to the number of attempts to try to get a fix for so that other jobs can be taken care of afterwards. TODO: convert it to interrupts?

--- sensorboxes/sbox-gps-test/ttn-credentials.h
+++ sensorboxes/sbox-gps-test/ttn-credentials.h
@@ -4,11 +4,13 @@
  * 
  */
 
+#define FILLME 0
+
 // application id 
-static const u1_t PROGMEM APPEUI[8]= { FILL ME };
+static const u1_t PROGMEM APPEUI[8]= { FILLME };
 
 // dev id 
-static const u1_t PROGMEM DEVEUI[8]= { FILL ME };
+static const u1_t PROGMEM DEVEUI[8]= { FILLME };
 
 // application key
-static const u1_t PROGMEM APPKEY[16] = { FILL ME };
+static const u1_t PROGMEM APPKEY[16] = { FILLME };
